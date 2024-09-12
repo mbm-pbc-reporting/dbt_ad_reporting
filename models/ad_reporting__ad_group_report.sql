2411d95bc15ff169c55977239d2cd07e866bcc31
mbm-pbc-reporting <pbc_reporting@mediabymother.com>
2024-09-12 13:33:30 +0530

    Update ad_reporting__ad_group_report.sql

--- models/ad_reporting__ad_group_report.sql
+++ models/ad_reporting__ad_group_report.sql
@@ -60,6 +60,48 @@ source_relation
 from 
 {{ ref('ttd_ads__custom_ad_group_report') }}
 --`pbc-reporting-dev.mother_ny_pbc_tradedesk_summary_dev.ttd_ads__custom_ad_group_report`
+union all    
+    
+SELECT 
+source_relation
+,date_day
+,'youtube' as platform
+,cast(account_id as string)
+,account_name
+,cast(campaign_id as string)
+,campaign_name
+,cast(ad_group_id as string)
+,ad_group_name
+,sum(clicks) as clicks
+,sum(impressions) as impressions
+,sum(spend) as spend
+,sum(conversions) as conversions    
+from 
+--`pbc-reporting-dev`.`mother_ny_pbc_youtube_summary_dev`.`youtube_ads__ad_report` 
+    {{ref('youtube_ads__ad_report')}}
+group by 1,2,3,4,5,6,7,8,9
+
+union all
+
+SELECT 
+source_relation
+,date_day
+,'performance_max' as platform
+,cast(account_id as string)
+,account_name
+,cast(campaign_id as string)
+,campaign_name
+,cast(ad_group_id as string)
+,ad_group_name
+,sum(clicks) as clicks
+,sum(impressions) as impressions
+,sum(spend) as spend
+,sum(conversions) as conversions    
+from   
+    {{rf('performance_max_ads__ad_report')}}
+--`pbc-reporting-dev`.`mother_ny_pbc_performance_max_summary_dev`.`performance_max_ads__ad_report` 
+group by 1,2,3,4,5,6,7,8,9
+   
 )
 
 select *
