d5597c04c5da49422eeef7ca6109e89fefd1ada0
mbm-pbc-reporting <pbc_reporting@mediabymother.com>
2024-10-21 15:39:25 +0530

    Update ad_reporting__account_report.sql

--- models/ad_reporting__account_report.sql
+++ models/ad_reporting__account_report.sql
@@ -48,7 +48,7 @@ source_relation
 ,impressions
 ,spend
 ,conversions    
-from {{ ref('ttd_ads__custom_account_report') }}
+from {{ ref('ttd_ads__custom_account_summary_report') }}
 union all    
     
 SELECT 
@@ -62,8 +62,8 @@ source_relation
 ,sum(spend) as spend
 ,sum(conversions) as conversions    
 from 
---`pbc-reporting-dev`.`mother_ny_pbc_youtube_summary_dev`.`youtube_ads__ad_report` 
-    {{ref('youtube_ads__ad_report')}}
+--`pbc-reporting-dev`.`mother_ny_pbc_youtube_summary_dev`.`youtube_ads__custom_ad_summary_report` 
+    {{ref('youtube_ads__custom_ad_summary_report')}}
 group by 1,2,3,4,5
 
 union all
@@ -79,8 +79,8 @@ source_relation
 ,sum(spend) as spend
 ,sum(conversions) as conversions    
 from   
-    {{ref('performance_max_ads__ad_report')}}
---`pbc-reporting-dev`.`mother_ny_pbc_performance_max_summary_dev`.`performance_max_ads__ad_report` 
+    {{ref('performance_max_ads__custom_ad_summary_report')}}
+--`pbc-reporting-dev`.`mother_ny_pbc_performance_max_summary_dev`.`performance_max_ads__custom_ad_summary_report` 
 group by 1,2,3,4,5
     
     )
